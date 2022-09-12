779eb6f083993c62d9c56d186de8fd6e27ae2596
vikas kumar <vikassaraimeer1999@gmail.com>
2022-09-12 19:11:28 +0530

    save

--- src/component/component.js
+++ src/component/component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './style.css'
-const Component = ({menudata}) => {}
+const Component = ({menudata}) => {
   return (
       <>
         <section className='main-container'>

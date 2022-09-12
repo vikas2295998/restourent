96a6db5bf4d9a9b27caba9a373be1ef8020d062f
vikas kumar <vikassaraimeer1999@gmail.com>
2022-09-12 18:31:57 +0530

    Updates

---
+++ precache-manifest.3020b153ebb0410d6717a6813d5cd226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f74224db4f91b4bcc48e",
+    "url": "/restourent/static/css/main.36212707.chunk.css"
+  },
+  {
+    "revision": "f74224db4f91b4bcc48e",
+    "url": "/restourent/static/js/main.f74224db.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/restourent/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/restourent/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "2e64d9e6926ced0e57fc",
+    "url": "/restourent/static/js/runtime~main.2e64d9e6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/restourent/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3debe288fefb65fc4dc27a583997fdc5",
+    "url": "/restourent/index.html"
+  }
+];
